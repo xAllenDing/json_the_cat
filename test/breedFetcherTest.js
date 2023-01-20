7813872262fe75f124bed9e067243c2799944c3c
Allen Ding <allen.ding@hotmail.com>
2023-01-19 19:49:28 -0800

    refactored and added mocha and chai to test

---
+++ test/breedFetcherTest.js
@@ -0,0 +1,27 @@
+const { fetchBreedDescription } = require('../breedFetcher');
+const { assert } = require('chai');
+
+describe('fetchBreedDescription', () => {
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+
+      // compare returned description
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+  it('returns an error message when invalid/non-existent breed is passed', (done) => {
+    fetchBreedDescription('abc', (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal("Breed does not exist", err);
+      assert.equal(null, desc);
+
+      done();
+    });
+  });
+});
